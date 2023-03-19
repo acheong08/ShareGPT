3c35b10bdbd7152aeb69c71bfff94874ff745fea
Antonio Cheong <acheong@student.dalat.org>
2023-03-19 23:59:15 +0800

    more type bugfixes

--- typings/types.go
+++ typings/types.go
@@ -3,30 +3,39 @@ package typings
 import "time"
 
 type BillingSubscription struct {
-	Object             string         `json:"object"`
-	HasPaymentMethod   bool           `json:"has_payment_method"`
-	Canceled           bool           `json:"canceled"`
-	CanceledAt         *time.Time     `json:"canceled_at"`
-	Delinquent         *bool          `json:"delinquent"`
-	AccessUntil        int64          `json:"access_until"`
-	SoftLimit          int            `json:"soft_limit"`
-	HardLimit          int            `json:"hard_limit"`
-	SystemHardLimit    int            `json:"system_hard_limit"`
-	SoftLimitUSD       float64        `json:"soft_limit_usd"`
-	HardLimitUSD       float64        `json:"hard_limit_usd"`
-	SystemHardLimitUSD float64        `json:"system_hard_limit_usd"`
-	Plan               Plan           `json:"plan"`
-	AccountName        string         `json:"account_name"`
-	PONumber           *string        `json:"po_number"`
-	BillingEmail       *string        `json:"billing_email"`
-	TaxIDs             *[]string      `json:"tax_ids"`
-	BillingAddress     BillingAddress `json:"billing_address"`
-	BusinessAddress    *string        `json:"business_address"`
+	Object             string          `json:"object"`
+	HasPaymentMethod   bool            `json:"has_payment_method"`
+	Canceled           bool            `json:"canceled"`
+	CanceledAt         *time.Time      `json:"canceled_at"`
+	Delinquent         *bool           `json:"delinquent"`
+	AccessUntil        int64           `json:"access_until"`
+	SoftLimit          int             `json:"soft_limit"`
+	HardLimit          int             `json:"hard_limit"`
+	SystemHardLimit    int             `json:"system_hard_limit"`
+	SoftLimitUSD       float64         `json:"soft_limit_usd"`
+	HardLimitUSD       float64         `json:"hard_limit_usd"`
+	SystemHardLimitUSD float64         `json:"system_hard_limit_usd"`
+	Plan               Plan            `json:"plan"`
+	AccountName        string          `json:"account_name"`
+	PONumber           *string         `json:"po_number"`
+	BillingEmail       *string         `json:"billing_email"`
+	TaxIDs             *[]string       `json:"tax_ids"`
+	BillingAddress     BillingAddress  `json:"billing_address"`
+	BusinessAddress    BusinessAddress `json:"business_address"`
 }
 
 type Plan struct {
 	Title string `json:"title"`
 	ID    string `json:"id"`
+}
+
+type BusinessAddress struct {
+	City       string `json:"city"`
+	Line1      string `json:"line1"`
+	Line2      string `json:"line2"`
+	State      string `json:"state"`
+	Country    string `json:"country"`
+	PostalCode string `json:"postal_code"`
 }
 
 type BillingAddress struct {
